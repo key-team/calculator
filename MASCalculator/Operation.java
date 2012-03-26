42ec4565fc57d9298ce68ea95a8523b605ed6898
sina saeedi <redshoghal@gmail.com>
2012-03-27 02:50:11 +0430

    Sina:operation class changes

--- MASCalculator/Operation.java
+++ MASCalculator/Operation.java
@@ -9,6 +9,54 @@ package MASCalculator;
  *
  * @author dell
  */
-class Operation {
-
+class Operation 
+{
+    private Error   err;
+    private AC      ac;
+    
+    public Operation(Error errParam, AC acParam)
+    {
+        this.err = errParam;
+        this.ac = acParam;
+    }
+    
+    public Operand sum(Operand op1, Operand op2)
+    {
+        Operand tempOperand = new Operand();
+        tempOperand.setValue( op1.getValue() + op2.getValue() );
+        return tempOperand;
+    }
+    
+    public Operand subtract(Operand op1, Operand op2)
+    {
+        Operand tempOperand = new Operand();
+        tempOperand.setValue( op1.getValue() - op2.getValue() );
+        return tempOperand;
+    }
+    
+    public Operand multiply(Operand op1, Operand op2)
+    {
+        Operand tempOperand = new Operand();
+        tempOperand.setValue( op1.getValue() * op2.getValue() );
+        return tempOperand;
+    }
+    
+    public Operand division(Operand op1, Operand op2)
+    {
+        Operand tempOperand = new Operand();
+        double buf;
+        try 
+        {
+            buf = op1.getValue() / op2.getValue();
+        }
+        catch (Exception e) 
+        {
+            err.setErrorCode(Constants.ERROR_DIVBYZERO);
+            return null;
+        }
+        
+        tempOperand.setValue(buf);
+        return tempOperand;
+    }
+    
 }
