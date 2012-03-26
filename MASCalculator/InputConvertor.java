18f67598f45b2c982bece9b3cbbc137a3b080ace
sina saeedi <redshoghal@gmail.com>
2012-03-27 00:32:44 +0430

    Sina: complete InputConvertor

--- MASCalculator/InputConvertor.java
+++ MASCalculator/InputConvertor.java
@@ -13,24 +13,34 @@ class InputConvertor {
 
     public static Operand toHex(String str)
     {
-       
-       
-                
+      int intValue = Integer.parseInt(str, 16);
+      double doubleValue = (double) intValue;
+      Operand tempOperand = new Operand(doubleValue);
+      return tempOperand;
     }
     
     public static Operand toOct(String str)
     {
-        
+      int intValue = Integer.parseInt(str, 8);
+      double doubleValue = (double) intValue;
+      Operand tempOperand = new Operand(doubleValue);
+      return tempOperand;
     }
     
     public static Operand toBin(String str)
     {
-        
+      int intValue = Integer.parseInt(str, 2);
+      double doubleValue = (double) intValue;
+      Operand tempOperand = new Operand(doubleValue);
+      return tempOperand;
     }
     
     public static Operand toDec(String str)
     {
-        
+      int intValue = Integer.parseInt(str, 10);
+      double doubleValue = (double) intValue;
+      Operand tempOperand = new Operand(doubleValue);
+      return tempOperand;
     }
     
 }
