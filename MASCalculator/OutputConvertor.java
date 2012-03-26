2d1661102b26a0481f2855a1774211d118d7d465
morteza Amzajerdi <ammorteza@yahoo.com>
2012-03-26 12:45:07 +0430

    morteza Amzajerdi:change inputconvertor class

--- MASCalculator/OutputConvertor.java
+++ MASCalculator/OutputConvertor.java
@@ -10,5 +10,20 @@ package MASCalculator;
  * @author dell
  */
 class OutputConvertor {
-
+    public static String toHex(Operand op1)
+    {
+        return String.format("%X", op1.getValue());
+    }
+    public static String toOct(Operand op1)
+    {
+        return String.format("%o", op1.getValue());
+    }
+    public static String toBin(Operand op1)
+    {
+        return String.format("%b", op1.getValue());
+    }
+    public static String toDec(Operand op1)
+    {
+        return String.format("%d", op1.getValue());
+    }
 }
