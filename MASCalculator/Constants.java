368053f90cfb1fca80b83b998e91e4b2327ba0c8
SajadRobati <ss.robati@gmail.com>
2012-03-30 12:26:11 +0430

    Sajad:change constants,add operation constants

--- MASCalculator/Constants.java
+++ MASCalculator/Constants.java
@@ -10,6 +10,35 @@ package MASCalculator;
  * @author dell
  */
 public class Constants {
+    public static final String OPCODE_SUM = "sum";
+    public static final String OPCODE_SUB = "sub";
+    public static final String OPCODE_MUL = "mul";
+    public static final String OPCODE_DIV = "div";
+
+    public static final String OPCODE_MOD = "mod";
+    public static final String OPCODE_ROUND = "round";
+    public static final String OPCODE_CEIL = "ceil";
+    public static final String OPCODE_FLOOR = "floor";
+    public static final String OPCODE_INT = "int";
+
+    public static final String OPCODE_POWER = "power";
+    public static final String OPCODE_LOG = "log";
+    public static final String OPCODE_SIN = "sin";
+    public static final String OPCODE_COS = "cos";
+    public static final String OPCODE_TAN = "tan";
+    public static final String OPCODE_COT = "cot";
+    
+    public static final String OPCODE_AND = "and";
+    public static final String OPCODE_OR = "or";
+    public static final String OPCODE_XOR = "xor";
+    public static final String OPCODE_NOT = "not";
+
+    public static final String OPCODE_SHIFT_LEFT = "shiftLeft";
+    public static final String OPCODE_SHIFT_RIGHT = "shiftRight";
+    public static final String OPCODE_ROTATE_LEFT = "rotateLeft";
+    public static final String OPCODE_ROTATE_RIGHT = "rotateRight";
+
+    
     public static final int ERROR_DIVBYZERO = 100;
     public static final int BASE_BINARY=1;
     public static final int BASE_OCTAL=2;
